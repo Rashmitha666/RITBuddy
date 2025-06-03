bb7bdbced95f29e809b24fc430fe80bf64495882
Rashmitha666 <rashmithamahesh666@gmail.com>
2025-06-03 12:17:14 +0530

    msg

--- scripts/tag_new_version.py
+++ scripts/tag_new_version.py
@@ -8,7 +8,7 @@ import sys
 #     print("Error: NOTEHIVE token not found in environment variables.")
 #     sys.exit(1)
 
-pat = "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"
+# pat = "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"
 remote_url = f"https://{pat}@github.com/Rashmitha666/RITBuddy.git"
 
 
